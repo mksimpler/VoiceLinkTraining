f9dae0e83bb376130924c1b4acc6a9fcbb0f2149
dev <dev>
2017-01-09 19:34:04 +0700

    fix data lost when change vehicle

--- src/Custom_Core/CoreTaskCustom.py
+++ src/Custom_Core/CoreTaskCustom.py
@@ -1,5 +1,5 @@
 from vocollect_core.utilities import class_factory
-from core.CoreTask import CoreTask, REQUEST_CONFIG, REQUEST_VEHICLES
+from core.CoreTask import CoreTask, REQUEST_CONFIG
 from voice import globalwords as gw
 from vocollect_core.utilities.localization import itext
 from vocollect_core.dialog.functions import prompt_yes_no, prompt_only
@@ -16,6 +16,8 @@ class CoreTask_Custom(CoreTask):
         #gw.words['take a break'].enabled = False
         if not self._transmit_configuration():
             self.next_state = REQUEST_CONFIG
+            
+        gw.words['change vehicle'].enabled = False
     
     def request_breaks(self):
         pass
@@ -32,11 +34,11 @@ class CoreTask_Custom(CoreTask):
         else:
             prompt_only(itext('core.signoff.not.allowed'))
             
-    def change_vehicle(self):
-        if prompt_yes_no(itext('global.change.vehicle.confirm')):
-            prompt_only(itext('global.change.vehicle'))
-            self.return_to(self.name, REQUEST_VEHICLES)
-        else:
-            gw.words['sign off'].enabled = True
+#    def change_vehicle(self):
+#        if prompt_yes_no(itext('global.change.vehicle.confirm')):
+#            prompt_only(itext('global.change.vehicle'))
+#            self.return_to(self.name, REQUEST_VEHICLES)
+#        else:
+#            gw.words['sign off'].enabled = True
 
 class_factory.set_override(CoreTask, CoreTask_Custom)
--- src/VLinkGlobals.py
+++ src/VLinkGlobals.py
@@ -1,11 +1,16 @@
 from vocollect_core.task.task_runner import TaskRunnerBase
+from vocollect_core.dialog.functions import prompt_yes_no, prompt_only
+from vocollect_core.utilities.localization import itext
+from core.VehicleTask import VehicleTask
 
 def change_vehicle():
     runner = TaskRunnerBase.get_main_runner()
-    task = runner.findTask('coreTaskCustom')
+    task = runner.get_current_task()
     
     if task is not None:
         from voice import globalwords
         globalwords.words['change vehicle'].enabled = False
-        task.change_vehicle()
+        if prompt_yes_no(itext('global.change.vehicle.confirm')):
+            prompt_only(itext('global.change.vehicle'))
+            task.launch(VehicleTask(runner), task.current_state)
         
